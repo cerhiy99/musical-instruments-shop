d437d1144ecb3bc427fc2d48ee6ce9af800a4290
sketsD <dima.stecun.99@gmail.com>
2025-02-18 14:32:27 +0200

    feat: update configs

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Додайте ваші власні налаштування тут
-
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
   // Приклад: відключення автоматичного підбору Webpack
   webpack5: false,
   images: {
