7067c8960c03cf4230870a44e445013ff61f0c89
sketsD <dima.stecun.99@gmail.com>
2025-03-12 04:43:22 +0200

    feat/added ineractivity for breadcrumb component

--- client/components/Header/TopicHeader.tsx
+++ client/components/Header/TopicHeader.tsx
@@ -1,14 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import "./TopicHeader.scss";
 
 export default function TopicHeader() {
+  const pathname = usePathname();
+  const segments = pathname.split("/").filter(Boolean).slice(1);
   return (
     <div className="topicHeader">
       <div className="topicHeader__container">
         <div className="topicHeader__title">
-          <p>Главная - Каталог</p>
-          <h2>Каталог</h2>
+          <nav>
+            <li>
+              <Link href={"/"}> Главная </Link>
+            </li>
+            {segments.map((segment, index) => {
+              const href = "/" + segments.slice(0, index + 1).join("/");
+              return (
+                <li key={href} className="">
+                  <span className=""> - </span>
+                  <Link href={href} className="">
+                    {decodeURIComponent(
+                      segment[0].toUpperCase() + segment.slice(1)
+                    )}
+                  </Link>
+                </li>
+              );
+            })}
+          </nav>
         </div>
+        <h2>
+          {segments[segments.length - 1][0].toUpperCase() +
+            segments[segments.length - 1].slice(1)}
+        </h2>
       </div>
     </div>
   );
