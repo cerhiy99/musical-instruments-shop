47b2e8fc9df4a40ea7f27f2d6716e24aafd6bf82
sketsD <dima.stecun.99@gmail.com>
2025-03-23 04:18:52 +0200

    feat/ history flow updated

--- client/app/[lang]/(with-user-sidebar)/dashboard/orders/history/page.tsx
+++ client/app/[lang]/(with-user-sidebar)/dashboard/orders/history/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import { Locale } from "@/i18n.config";
 import "./Histroy.scss";
 import Link from "next/link";
 import { getDictionary } from "@/lib/dictionary";
+import { useState } from "react";
 
-const orders = [
+const MockOrders = [
   {
     id: 222689,
     date: "15.03.2023",
@@ -12,21 +15,21 @@ const orders = [
     currency: "грн",
   },
   {
-    id: 222689,
+    id: 222629,
     date: "15.03.2023",
     items: 3,
     total: 4650,
     currency: "грн",
   },
   {
-    id: 222689,
+    id: 222619,
     date: "15.03.2023",
     items: 3,
     total: 4650,
     currency: "грн",
   },
   {
-    id: 222689,
+    id: 112689,
     date: "15.03.2023",
     items: 3,
     total: 4650,
@@ -34,44 +37,66 @@ const orders = [
   },
 ];
 
-export default async function Histroy({
+export default function Histroy({
   params: { lang },
 }: {
   params: { lang: Locale };
 }) {
   // const { aside } = await getDictionary(lang);
 
+  const [orders, setOtrders] = useState(MockOrders);
+  const handleRemoveOrder = (orderID: number) => {
+    setOtrders((prevOrders) =>
+      prevOrders.filter((order) => order.id !== orderID)
+    );
+  };
+
   return (
     <div className="orders">
       <h3>Мои заказы</h3>
-      {orders.map((order, index) => (
-        <>
-          <div key={index} className="orders-item">
-            <p>
-              Замовлення №{order.id} від {order.date}, {order.items} товари на
-              суму {order.total} {order.currency}.
-            </p>
+      {orders.length === 0 ? (
+        <div className="orders-none">
+          <div className="orders-item">
+            <p>Замовлення відсутні.</p>
           </div>
-          <div className="orders-buttons">
-            <div className="orders-buttons__left">
-              <Link
-                href={`history/${order.id}`}
-                className="orders-buttons--details blue-orders-button"
-              >
-                Детальніше про замовлення
-              </Link>
-              <button className="orders-buttons--repeat blue-orders-button">
-                Повторити замовлення
-              </button>
-            </div>
-            <div className="orders-buttons__right">
-              <button className="orders-buttons--cancel">
-                Скасувати замовлення
-              </button>
-            </div>
+          <div className="orders-item--link">
+            <Link href={`/${lang}/catalog`} />
+            Перейти в каталог
           </div>
-        </>
-      ))}
+        </div>
+      ) : (
+        orders.map((order, index) => (
+          <>
+            <div key={index} className="orders-item">
+              <p>
+                Замовлення №{order.id} від {order.date}, {order.items} товари на
+                суму {order.total} {order.currency}.
+              </p>
+            </div>
+            <div className="orders-buttons">
+              <div className="orders-buttons__left">
+                <Link
+                  href={`history/${order.id}`}
+                  className="orders-buttons--details blue-orders-button"
+                >
+                  Детальніше про замовлення
+                </Link>
+                <button className="orders-buttons--repeat blue-orders-button">
+                  Повторити замовлення
+                </button>
+              </div>
+              <div className="orders-buttons__right">
+                <button
+                  className="orders-buttons--cancel"
+                  onClick={() => handleRemoveOrder(order.id)}
+                >
+                  Скасувати замовлення
+                </button>
+              </div>
+            </div>
+          </>
+        ))
+      )}
     </div>
   );
 }
